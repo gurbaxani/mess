c9d047ea65a821f33cfa913e4816d0c68074a61a
Ashwini Gurbaxani <98401812+gurbaxani@users.noreply.github.com>
2026-04-15 16:35:52 +0530

    Initialize

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href="https://fav.farm/🍽️" />
+	<title>Mess Menu</title>
+</svelte:head>
+{@render children()}
